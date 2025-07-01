b9f5667b25556059079d6b3c2a67523eb6dd6b37
Jan Patrick Palacio <janpatrick.dev@gmail.com>
2025-07-01 09:34:57 +0800

    implemented navigation bar mobile view + fixed home page mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className='mx-auto [&>section]:px-4'>
-      <section className='flex flex-col items-center justify-center gap-2 bg-gradient-to-r from-blue-700 to-cyan-200 to-80% py-28 text-center text-white'>
-        <h1 className='text-6xl font-bold'>Gawa na &apos;yan!</h1>
-        <p className='text-lg text-white/80'>Find trusted Filipino talent for any project, big or small.</p>
+      <section className='flex flex-col items-center justify-center gap-2 bg-gradient-to-r from-blue-700 to-cyan-200 py-28 text-center text-white sm:to-80%'>
+        <h1 className='text-4xl font-bold sm:text-6xl'>Gawa na &apos;yan!</h1>
+        <p className='text-white/80 sm:text-lg'>Find trusted Filipino talent for any project, big or small.</p>
         <div className='mt-8 flex w-full flex-col items-center justify-center gap-4 sm:flex-row'>
           <Input
-            className='w-full rounded-full bg-white px-4 py-6 text-black sm:max-w-[480px]'
+            className='w-full rounded-full bg-white px-4 py-6 text-sm text-black sm:max-w-[480px]'
             placeholder='What service do you need? (e.g. Logo Design)'
           />
           <Button className='w-full rounded-full bg-yellow-300 px-8 py-6 font-semibold text-black sm:w-auto'>
@@ -25,8 +25,8 @@ export default function Home() {
       </section>
       <section>
         <div className='container mx-auto flex flex-col items-center justify-center gap-2 py-28'>
-          <h2 className='text-4xl font-bold'>Explore by Category</h2>
-          <p className='text-lg opacity-80'>Find the right professional for your needs.</p>
+          <h2 className='text-center text-2xl font-bold sm:text-left sm:text-4xl'>Explore by Category</h2>
+          <p className='text-center opacity-80 sm:text-left sm:text-lg'>Find the right professional for your needs.</p>
           <div className='mt-8 grid w-full gap-4 sm:grid-cols-2 lg:grid-cols-4'>
             <ExploreCard
               icon={<Monitor size={32} />}
@@ -53,7 +53,7 @@ export default function Home() {
       </section>
       <section className='flex justify-center bg-neutral-50'>
         <div className='container flex flex-col gap-8 py-20'>
-          <h2 className='text-3xl font-bold'>Local Gigs in Angeles</h2>
+          <h2 className='text-center text-2xl font-bold sm:text-left sm:text-3xl'>Local Gigs in Angeles</h2>
           <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
             <ServiceCard title='Condo Cleaning for 2-Bedrooms' price={1200} />
             <ServiceCard title='Condo Cleaning for 2-Bedrooms' price={1200} />
@@ -68,8 +68,8 @@ export default function Home() {
       </section>
       <section className='flex justify-center'>
         <div className='container flex flex-col items-center justify-center gap-2 py-20'>
-          <h2 className='text-3xl font-bold'>Getting Things Done is Easy</h2>
-          <p>In just a few simple steps.</p>
+          <h2 className='text-center text-2xl font-bold sm:text-left sm:text-3xl'>Getting Things Done is Easy</h2>
+          <p className='text-center sm:text-left'>In just a few simple steps.</p>
           <div className='mt-16 grid place-items-start gap-20 sm:grid-cols-3'>
             <StepCard
               title='Search & Find'
@@ -91,7 +91,7 @@ export default function Home() {
       </section>
       <section className='flex justify-center bg-neutral-50'>
         <div className='container flex flex-col items-center justify-center gap-2 py-20'>
-          <h2 className='text-3xl font-bold'>Meet Our Top Raketeros</h2>
+          <h2 className='text-2xl font-bold sm:text-3xl'>Meet Our Top Raketeros</h2>
           <div className='mt-12 grid w-full gap-8 lg:grid-cols-3'>
             <RaketeroCard firstName='John' lastName='Doe' jobTitle='UX Designer' rating={4.9} reviews={20} />
             <RaketeroCard firstName='John' lastName='Doe' jobTitle='UX Designer' rating={4.9} reviews={20} />
@@ -101,20 +101,20 @@ export default function Home() {
       </section>
       <section className='flex justify-center bg-gradient-to-r from-yellow-200 to-orange-500'>
         <div className='container flex flex-col items-center py-20 text-white'>
-          <h2 className='text-3xl font-bold'>Ready to Get Started?</h2>
-          <p className='mt-2 max-w-[512px] text-center text-white/80'>
+          <h2 className='text-2xl font-bold sm:text-3xl'>Ready to Get Started?</h2>
+          <p className='mt-2 max-w-[512px] text-center text-sm text-white/80 sm:text-base'>
             Join our community of clients and talented Raketeros today. Your next project is just a few clicks away.
           </p>
-          <div className='mt-8 flex items-center gap-4'>
+          <div className='mt-8 flex w-full flex-col items-center gap-4 text-center sm:w-auto sm:flex-row sm:text-left'>
             <Link
               href='#'
-              className='rounded-full bg-white px-8 py-3 text-lg font-bold text-black transition-all hover:scale-105 hover:bg-white/80'
+              className='w-full rounded-full bg-white px-8 py-3 font-bold text-black transition-all hover:scale-105 hover:bg-white/80 sm:w-auto sm:text-lg'
             >
               Find a Service
             </Link>
             <Link
               href='#'
-              className='rounded-full bg-yellow-300 px-8 py-3 text-lg font-bold text-black transition-all hover:scale-105 hover:bg-yellow-400'
+              className='w-full rounded-full bg-yellow-300 px-8 py-3 font-bold text-black transition-all hover:scale-105 hover:bg-yellow-400 sm:w-auto sm:text-lg'
             >
               Become a Raketero
             </Link>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,36 +1,104 @@
 import Link from 'next/link';
+import { Menu } from 'lucide-react';
+import { Drawer, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer';
 
 export default function Navbar() {
   return (
-    <nav className='sticky top-0 z-50 flex w-full items-center justify-center bg-white/70 py-3 text-black/80 backdrop-blur-md'>
-      <div className='container flex items-center justify-between'>
-        <Link href='/' className='text-xl font-bold'>
-          <span>Gawa</span>
-          <span className='text-blue-500'>Link</span>
-        </Link>
-        <div className='flex items-center justify-center gap-4'>
-          <Link href='#' className='rounded-lg px-4 py-2 text-sm transition-colors hover:bg-neutral-100'>
-            Find Services
-          </Link>
-          <Link href='#' className='rounded-lg px-4 py-2 text-sm transition-colors hover:bg-neutral-100'>
-            How it Works
-          </Link>
-          <Link href='#' className='rounded-lg px-4 py-2 text-sm transition-colors hover:bg-neutral-100'>
-            Become a Raketero
-          </Link>
-        </div>
-        <div className='flex items-center justify-center gap-4'>
-          <Link href='#' className='text-sm'>
-            Login
-          </Link>
-          <Link
-            href='#'
-            className='rounded-full bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-blue-600'
-          >
-            Sign Up
-          </Link>
-        </div>
-      </div>
+    <nav className='sticky top-0 z-50 flex w-full items-center justify-center bg-white/70 px-4 py-3 text-black/80 backdrop-blur-md lg:px-0'>
+      <NavbarDesktop />
+      <NavbarMobile />
     </nav>
+  );
+}
+
+function NavbarDesktop() {
+  return (
+    <div className='container hidden items-center justify-between lg:flex'>
+      <Link href='/' className='text-xl font-bold'>
+        <span>Gawa</span>
+        <span className='text-blue-500'>Link</span>
+      </Link>
+      <div className='flex items-center justify-center gap-4'>
+        <Link href='#' className='rounded-lg px-4 py-2 text-sm transition-colors hover:bg-neutral-100'>
+          Find Services
+        </Link>
+        <Link href='#' className='rounded-lg px-4 py-2 text-sm transition-colors hover:bg-neutral-100'>
+          How it Works
+        </Link>
+        <Link href='#' className='rounded-lg px-4 py-2 text-sm transition-colors hover:bg-neutral-100'>
+          Become a Raketero
+        </Link>
+      </div>
+      <div className='flex items-center justify-center gap-4'>
+        <Link href='#' className='text-sm'>
+          Login
+        </Link>
+        <Link
+          href='#'
+          className='rounded-full bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-blue-600'
+        >
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+function NavbarMobile() {
+  return (
+    <div className='container flex items-center justify-between lg:hidden'>
+      <Link href='/' className='text-xl font-bold'>
+        <span>Gawa</span>
+        <span className='text-blue-500'>Link</span>
+      </Link>
+      <Drawer direction='left'>
+        <DrawerTrigger>
+          <Menu />
+        </DrawerTrigger>
+        <DrawerContent className='px-1'>
+          <DrawerHeader>
+            <DrawerTitle>
+              <Link href='/' className='text-xl font-bold'>
+                <span>Gawa</span>
+                <span className='text-blue-500'>Link</span>
+              </Link>
+            </DrawerTitle>
+          </DrawerHeader>
+          <div className='flex flex-col gap-1'>
+            <Link
+              href='#'
+              className='rounded-md px-4 py-2 font-semibold text-neutral-600 transition-colors active:bg-blue-100 active:text-blue-600'
+            >
+              Find Services
+            </Link>
+            <Link
+              href='#'
+              className='rounded-md px-4 py-2 font-semibold text-neutral-600 transition-colors active:bg-blue-100 active:text-blue-600'
+            >
+              How it Works
+            </Link>
+            <Link
+              href='#'
+              className='rounded-md px-4 py-2 font-semibold text-neutral-600 transition-colors active:bg-blue-100 active:text-blue-600'
+            >
+              Become a Raketero
+            </Link>
+          </div>
+          <DrawerFooter>
+            <div className='flex w-full items-center justify-center gap-2 text-center'>
+              <Link href='#' className='w-full text-sm'>
+                Login
+              </Link>
+              <Link
+                href='#'
+                className='w-full rounded-full bg-blue-500 px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-blue-600'
+              >
+                Sign Up
+              </Link>
+            </div>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </div>
   );
 }
