b059d7555bf24e30a4829c3c82b938690f4c5d1e
Jan Patrick Palacio <janpatrick.dev@gmail.com>
2025-07-02 10:47:01 +0800

    added nextjs-toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider attribute='class' defaultTheme='light' enableSystem disableTransitionOnChange>
+          <NextTopLoader showSpinner={false} />
           {children}
         </ThemeProvider>
       </body>
